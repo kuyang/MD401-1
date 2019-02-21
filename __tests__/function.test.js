1dc3fa1191038d41d11cb7c772303e12f63f8ad8
Greg Hsieh <greg.hsieh@gmail.com>
2019-02-21 14:30:07 -0500

    saving all 3 samples

--- __tests__/function.test.js
+++ __tests__/function.test.js
@@ -5,3 +5,11 @@ const funcs = require('../function');
 test('a + b should equal the correct sum', () => {
     expect(funcs.add(2, 3)).toBe(5);
 })
+
+test('a - b should equals the correct subtraction', () => {
+    expect(funcs.sub(5, 2)).toBe(3);
+})
+
+test('Should log "Hello welcome to MD401!"', () => {
+    expect(funcs.greeting()).toBe('Hello welcome to MD401!')
+})
--- function.js
+++ function.js
@@ -5,3 +5,9 @@ const funcs = module.exports = {};
 funcs.add = (a,b) => {
     return a+b
 } 
+funcs.sub = (a,b) => {
+    return a-b
+}
+funcs.greeting = () => {
+    return 'Hello welcome to MD401!'
+}
